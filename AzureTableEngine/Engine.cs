9e4d7754712f5879bf6d406f6c3373d959ed2b7c
Wizmann <mail.kuuy@gmail.com>
2018-01-18 01:16:56 +0800

    create new engines

---
+++ AzureTableEngine/Engine.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using Rotown;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rotown.Models;
+
+namespace AzureTableEngine
+{
+    public class Engine<T> : IEngine<T>
+        where T : Model<T>, new()
+    {
+        public Task Delete(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PartialResult<T>> QuerySegmented(T low, T high, int take, string continuationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Retrieve(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
